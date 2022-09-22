6752287b1349901e407328f9ace589a3dc99b33c
Patrickgomesrankmyapp <114117002+Patrickgomesrankmyapp@users.noreply.github.com>
2022-09-22 10:10:06 -0300

    Update mvp_interativo.py

--- mvp_interativo.py
+++ mvp_interativo.py
@@ -12,10 +12,6 @@ from shapely.geometry import Point
 st.title('MVP - Interactive GeoLocation')
 
 st.file_uploader('Dado de relogios em csv')
-df_relogio = pd.read_csv(arquivo_input)
-df_relogio['geometry'] = [Point(xy) for xy in zip(df_relogio['lng'], df_relogio['lat'])]
-df_relogio = geopandas.GeoDataFrame(df_relogio)
-
 
 osmnx = osmnx()
 
@@ -33,6 +29,11 @@ lista_opcoes_selecionadas = st.multiselect('Selecione seu filtro', lista_opcoes)
 
 
 df_localidade = dataframe[dataframe[tipo].isin(lista_opcoes_selecionadas)]
+
+
+df_relogio = pd.read_csv(arquivo_input)
+df_relogio['geometry'] = [Point(xy) for xy in zip(df_relogio['lng'], df_relogio['lat'])]
+df_relogio = geopandas.GeoDataFrame(df_relogio)
 
 dist_dict = {}
 for i, v in df.iterrows():
